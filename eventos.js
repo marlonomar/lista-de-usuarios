a1ded45b16437a7de2e903b65d77faac1ca4ef32
Garcia <marlon.omar.g.garcia@accenture.com>
2019-01-28 17:06:08 -0300

    agregue comentarios

--- eventos.js
+++ eventos.js
@@ -61,28 +61,28 @@ $(function(){
     /*---------filtro-----------*/
     
     obtenerdatos();
-    
+    // funcion principal
     function obtenerdatos (){
         var contenido_fila;
         var coinsidencia;
         var exp;
         var codigoAscci;
-    
-        $("#Buscar").keyup(function(){
-            if (!checkTeclaDel(event)) {
+    //filtrador de la barra de busqueda al ingresar un valor 
+        $("#Buscar").keyup(function(){// keyup se ejecuta cuando se ingresa un valor y se suelta el cursor
+            if (!checkTeclaDel(event)) {// check de la tecla del 
                 if ($(this).val().length>=1)
                 filtrar($(this).val());
             } 
         });
-    
+    // filtro 
         function filtrar(cadena){
             $('tbody tr').each(function(){
                 var todos = []
-                todos.push($(this).text())
+                todos.push($(this).text())// El método push () agrega nuevos elementos al final de una matriz y devuelve la nueva longitud.
                                             
                 $(this).removeClass('ocultar');
-                contenido_fila=todos[0];
-                exp= new RegExp(cadena,'gi');
+                contenido_fila=todos[0];// junta todos los texto en un solo elemento para su busqueda
+                exp= new RegExp(cadena,'gi');//El constructor RegExpcrea un objeto de expresión regular para que coincida con el texto con un patrón
                 coinsidencia=contenido_fila.match(exp);
                 if (coinsidencia!=null) {
                     
@@ -91,6 +91,7 @@ $(function(){
                 }
             });
         }   
+        // cuando consigue coincidencias oculta los demas objetos
         function mostrarfilas(){
             $("table tbody tr").each(function(){
                 $(this).removeClass('ocultar');
@@ -98,7 +99,7 @@ $(function(){
         }
 
         function checkTeclaDel(e){
-            codigoAscci = e.which;
+            codigoAscci = e.which;//whiches una propiedad del eventobjeto, que la mayoría de la gente etiqueta como een sus controladores de eventos. Contiene el código clave de la tecla que se presionó para desencadenar el evento (por ejemplo: keydown, keyup).
             if (codigoAscci==8) {
                 if ($("#Buscar").val().length>=1) {
                     filtrar($("#Buscar").val());
